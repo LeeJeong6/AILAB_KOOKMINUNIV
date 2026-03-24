85a7e1e6f9bd9d8ec22c549840a0aa68687764ae
LeeJeong6 <bill0914@naver.com>
2026-03-24 13:24:56 +0900

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// API Configuration
-const API_CONFIG = {
-  API_BASE_URL: window.location.hostname === 'localhost'
-    ? 'http://localhost:3000/api'
-    : 'https://ailab-kookminuniv-ez9h2o4gr-02707s-projects.vercel.app/api'
-};
